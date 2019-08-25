466b6172ab6a15385477ecfc9c6079e34b410c7b
ChickenFeet (Cindy Zhu) <45746066+cindyzhu0830@users.noreply.github.com>
2019-08-25 10:38:27 +0800

    Add files via upload

--- src/main.cpp
+++ src/main.cpp
@@ -5,35 +5,22 @@
 #include <igl/png/writePNG.h>
 #include <igl/png/readPNG.h>
 
-// This function is called every time a keyboard button is pressed
 
 
 int main(int argc, char *argv[])
 {
-  // Load a mesh in OFF format
-  
+	Eigen::MatrixXd V;
+	Eigen::MatrixXi F;
+	igl::readOFF(TUTORIAL_SHARED_PATH "/bunny.off", V, F);
 
-  
+	std::cerr << "Press 1 to render the scene and save it in a png." << std::endl;
+	std::cerr << "Press 2 to load the saved png and use it as a texture." << std::endl;
 
-  // Plot the mesh and register the callback
-  igl::opengl::glfw::Viewer viewer;
-  viewer.load_mesh_from_file(std::string(TUTORIAL_SHARED_PATH) + "/cube.obj");
-  unsigned int left_view, right_view;
-
-  viewer.callback_init = [&](igl::opengl::glfw::Viewer &)
-  {
-	  viewer.core().viewport = Eigen::Vector4f(0, 0, 640, 800);
-	  left_view = viewer.core_list[0].id;
-	  right_view = viewer.append_core(Eigen::Vector4f(640, 0, 640, 800));
-	  return false;
-  };
-
-  viewer.callback_post_resize = [&](igl::opengl::glfw::Viewer &v, int w, int h) {
-	  v.core(left_view).viewport = Eigen::Vector4f(0, 0, w / 2, h);
-	  v.core(right_view).viewport = Eigen::Vector4f(w / 2, 0, w - (w / 2), h);
-	  return true;
-  };
-  viewer.launch();
+	// Plot the mesh and register the callback
+	igl::opengl::glfw::Viewer viewer;
+	viewer.callback_key_down = &key_down;
+	viewer.data().set_mesh(V, F);
+	viewer.launch();
 }
 
 
