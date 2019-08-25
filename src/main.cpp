1a9e22cc3a3f7afd1c462b984fa169ef8f36b1e2
ChickenFeet (Cindy Zhu) <45746066+cindyzhu0830@users.noreply.github.com>
2019-08-25 10:34:22 +0800

    Canceled button and multipleviewports

--- src/main.cpp
+++ src/main.cpp
@@ -6,35 +6,13 @@
 #include <igl/png/readPNG.h>
 
 // This function is called every time a keyboard button is pressed
-bool key_down(igl::opengl::glfw::Viewer& viewer, unsigned char key, int modifier)
-{
-  if (key == '1')
-  {
-    // Allocate temporary buffers
-    Eigen::Matrix<unsigned char,Eigen::Dynamic,Eigen::Dynamic> R(1280,800);
-    Eigen::Matrix<unsigned char,Eigen::Dynamic,Eigen::Dynamic> G(1280,800);
-    Eigen::Matrix<unsigned char,Eigen::Dynamic,Eigen::Dynamic> B(1280,800);
-    Eigen::Matrix<unsigned char,Eigen::Dynamic,Eigen::Dynamic> A(1280,800);
 
-    // Draw the scene in the buffers
-    viewer.core().draw_buffer(
-      viewer.data(),false,R,G,B,A);
-
-    // Save it to a PNG
-    igl::png::writePNG(R,G,B,A,"out.png");
-  }
-
-
-
-  return false;
-}
 
 int main(int argc, char *argv[])
 {
   // Load a mesh in OFF format
   
 
-  std::cerr << "Press 1 to render the scene and save it in a png." << std::endl;
   
 
   // Plot the mesh and register the callback
@@ -61,7 +39,7 @@ int main(int argc, char *argv[])
 
 
 
-
+ 
 
 
 
